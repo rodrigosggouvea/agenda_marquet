6bc7fba639a451561023132780cbd2d2195efa88
Rodrigo Gouvea <rodrigosggouvea@gmail.com>
2012-11-11 14:59:15 -0200

    levando pro github

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
+//= require jquery.carousel.min	
+//= require jquery.orbit-1.2.3.min
 //= require rails.validations
 //= require_tree .
 
