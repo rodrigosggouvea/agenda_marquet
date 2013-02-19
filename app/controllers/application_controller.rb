2cf8120a01a567a43d5704676baba193ada76cdb
Rodrigo Gouvea <rodrigosggouvea@gmail.com>
2013-02-19 10:43:38 -0300

    correções de login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,4 +30,21 @@ class ApplicationController < ActionController::Base
     end
     redirect_to contact_url
   end
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  # Overwriting the sign_up redirect path method
+  def after_sign_up_path_for(resource_or_scope)
+    root_path
+  end
+
+  # Overwriting the sign_up redirect path method
+  def after_sign_in_path_for(resource_or_scope)
+    root_path
+  end
 end
