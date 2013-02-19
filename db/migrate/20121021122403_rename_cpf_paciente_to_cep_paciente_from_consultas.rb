d543c16435cd6ad511751ca1c11c7b15da4326d3
Rodrigo Gouvea <rodrigosggouvea@gmail.com>
2013-02-19 11:51:29 -0300

    adicionada data de nascimento a consulta

--- db/migrate/20121021122403_rename_cpf_paciente_to_cep_paciente_from_consultas.rb
+++ db/migrate/20121021122403_rename_cpf_paciente_to_cep_paciente_from_consultas.rb
@@ -1,9 +1,4 @@
 class RenameCpfPacienteToCepPacienteFromConsultas < ActiveRecord::Migration
-  def up
-  	rename_column :consultas, :cpf_paciente, :cep_paciente
-  end
-
-  def down
-  	rename_column :consultas, :cep_paciente, :cpf_paciente
+  def change
   end
 end
